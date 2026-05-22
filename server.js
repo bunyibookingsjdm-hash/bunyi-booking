2e26192b7475c0cb0bfe9d3a9e6cd175a5795ca8
Bunyi Booking <bunyibooking.sjdm@gmail.com>
2026-05-22 22:23:58 +0800

    Revert session store

--- server.js
+++ server.js
@@ -31,13 +31,7 @@ app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, 'favicon
 app.get('/favicon.png', (req, res) => res.sendFile(path.join(__dirname, 'favicon.png')));
 
 const session = require('express-session');
-const {FirebaseStore} = require('connect-session-firebase');
-app.use(session({
-  store: new FirebaseStore({ database: db }),
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 
 // ===== CLOUDINARY =====
 const cloudinary = require('cloudinary').v2;
