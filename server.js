4f75b4dfa65d79906cea6518bee4bbdf3130f611
Bunyi Booking <bunyibooking.sjdm@gmail.com>
2026-05-22 16:07:21 +0800

    Save server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const multer = require('multer');
 
 // ===== FIREBASE =====
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
+);
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
 
